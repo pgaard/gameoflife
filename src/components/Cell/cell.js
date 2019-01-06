6b4c0b586128e6de07c1df70c2af88002dcbae35
Peter Gaard <pgaard@gmail.com>
2019-01-05 18:51:49 -0600

    Faster

--- src/components/Cell/cell.js
+++ src/components/Cell/cell.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './cell.module.css';
 
-const Cell = React.memo((props) => 
-    <div onClick={props.click} 
-        className={props.alive ? classes.CellActive : classes.CellInactive}>
-    </div>)
+class Cell extends Component
+{
+    shouldComponentUpdate(nextProps){
+        return nextProps.alive !== this.props.alive;
+    }
+
+    render(){
+        return(
+            <div onClick={this.props.click} 
+                className={this.props.alive ? classes.CellActive : classes.CellInactive}>
+            </div>
+        );
+    }
+}
 
 export default Cell;

84dfb3ea4ba03df57db7f97a5b8b090b7505a2e8
Peter Gaard <pgaard@gmail.com>
2019-04-19 18:49:12 -0500

    build

---
+++ build/precache-manifest.6f49ad732ba4b184254ac81b0be58d9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "74778945af6ecfd74e9e",
+    "url": "/static/js/main.74778945.chunk.js"
+  },
+  {
+    "revision": "597d2e755385d1ba5aba",
+    "url": "/static/js/1.597d2e75.chunk.js"
+  },
+  {
+    "revision": "74778945af6ecfd74e9e",
+    "url": "/static/css/main.2462ddc5.chunk.css"
+  },
+  {
+    "revision": "1b1ec54cba8143e905dc09153d54cf64",
+    "url": "/index.html"
+  }
+];

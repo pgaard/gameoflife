59ad625a678b856c226e10ca155eeab84767f30a
Peter Gaard <pgaard@gmail.com>
2019-04-19 19:32:47 -0500

    build relative paths

---
+++ build/precache-manifest.65563eac57697035ccda1890ebddc1e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "05153b8be0b896378162",
+    "url": "./static/js/main.05153b8b.chunk.js"
+  },
+  {
+    "revision": "597d2e755385d1ba5aba",
+    "url": "./static/js/1.597d2e75.chunk.js"
+  },
+  {
+    "revision": "05153b8be0b896378162",
+    "url": "./static/css/main.2462ddc5.chunk.css"
+  },
+  {
+    "revision": "f56396bc4457d64eab4f58b0f7588e14",
+    "url": "./index.html"
+  }
+];
